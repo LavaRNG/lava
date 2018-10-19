fac2bba23741561c9db480c04deb30058f22fe9b
Ian lapham <ianlapham@gmail.com>
2018-10-19 13:43:42 -0400

    fresh UI start

---
+++ lava-ui/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component} from "react";
+import "./App.css";
+import Header from "./components/Header.js";
+ 
+class App extends Component{
+ 
+  constructor(props){
+    super(props)
+    this.state = {
+    }
+  }
+ 
+  render(){
+    return(
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
+}
+ 
+export default App;
